4fe530bb49dab8c33ab7c36bdbbea77afbdb95e0
seoyun-lee <20212025@o365.hanbat.ac.kr>
2023-08-23 02:53:22 +0900

    Topping 클래스

---
+++ src/ShrimpTopping.java
@@ -0,0 +1,14 @@
+public class ShrimpTopping extends ToppingDecorator {
+    public ShrimpTopping(Food baseFood) {
+        super(baseFood);
+        name = "새우 " + baseFood.getName();
+    }
+
+    public String getDescription() {
+        return baseFood.getDescription() + " + 새우";
+    }
+
+    public int cost() {
+        return baseFood.cost() + 1000;
+    }
+}
