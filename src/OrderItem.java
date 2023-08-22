e1aa2a59541e06e01df83a18f76fe87f1ca8278b
seoyun-lee <20212025@o365.hanbat.ac.kr>
2023-08-23 02:54:44 +0900

    주문된 음식 항목 표현 클래스

--- src/OrderItem.java
+++ src/OrderItem.java
@@ -1,6 +1,6 @@
 public class OrderItem {
-    private Food food;
-    private int quantity;
+    private Food food; // 주문된 음식을 나타내는 Food 객체
+    private int quantity; // 주문된 음식의 수량
 
     public OrderItem(Food food, int quantity) {
         this.food = food;
@@ -8,18 +8,18 @@ public class OrderItem {
     }
 
     public String getName() {
-        return food.getName();
+        return food.getName(); // 음식 이름 반환
     }
 
     public String getDescription() {
         return food.getDescription();
-    }
+    } // 음식 설명 반환
 
     public int getQuantity() {
         return quantity;
-    }
+    } // 음식 수량 반환
 
     public int getTotalCost() {
         return food.cost() * quantity;
-    }
+    } // 음식의 총 가격 반환
 }
