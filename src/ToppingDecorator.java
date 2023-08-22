0253f6a0c3cf15b2413781cc815d40b8d0d68988
seoyun-lee <20212025@o365.hanbat.ac.kr>
2023-08-23 02:53:00 +0900

    Decorator Pattern 을 사용한 Topping 추가 관리 클래스

--- src/ToppingDecorator.java
+++ src/ToppingDecorator.java
@@ -1,9 +1,9 @@
 public abstract class ToppingDecorator extends Food {
-    protected Food baseFood;
+    protected Food baseFood; // 토핑을 추가할 기본 음식
 
     public ToppingDecorator(Food baseFood) {
         this.baseFood = baseFood;
     }
 
-    public abstract String getDescription();
+    public abstract String getDescription(); // 토핑 추가에 대한 설명을 반환하는 추상 메서드
 }
