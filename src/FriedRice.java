89b077b42a292f4428739b20e94646627a854496
seoyun-lee <20212025@o365.hanbat.ac.kr>
2023-08-23 01:13:58 +0900

    Food 클래스

---
+++ src/FriedRice.java
@@ -0,0 +1,10 @@
+public class FriedRice extends Food {
+    public FriedRice() {
+        name = "볶음밥";
+        description = "기본 계란 볶음밥";
+    }
+
+    public int cost() {
+        return 3800;
+    }
+}
