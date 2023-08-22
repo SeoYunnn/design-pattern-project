dfb48a86bc8ec86d06a2f41ade253b00f823fda6
seoyun-lee <20212025@o365.hanbat.ac.kr>
2023-08-23 01:11:11 +0900

    Topping 클래스

---
+++ src/EggTopping.java
@@ -0,0 +1,14 @@
+public class EggTopping extends ToppingDecorator {
+    public EggTopping(Food baseFood) {
+        super(baseFood);
+        name = "계란 " + baseFood.getName();
+    }
+
+    public String getDescription() {
+        return baseFood.getDescription() + " + 계란 토핑";
+    }
+
+    public int cost() {
+        return baseFood.cost() + 500;
+    }
+}
